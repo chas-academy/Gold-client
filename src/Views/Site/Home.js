ca781c5a757d098d33bcc3079125884e8a8a6db2
Oleg-Lopes <oleg.lopes@chasacademy.se>
2018-04-12 14:41:57 +0200

    test commit

--- src/Views/Site/Home.js
+++ src/Views/Site/Home.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
-import ReactLogo from '../../Assets/Images/react-logo.svg'
-import { PageTitle } from '../../Lib/Common/Views'
+import React, { Component } from "react";
+import ReactLogo from "../../Assets/Images/react-logo.svg";
+import { PageTitle } from "../../Lib/Common/Views";
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div className="home-view">
-        <header className="home-view-header">
-          <img src={ReactLogo} className="home-view-logo" alt="React Logo" />
-          <PageTitle />
-        </header>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="home-view">
+                <header className="home-view-header">
+                    <img
+                        src={ReactLogo}
+                        className="home-view-logo"
+                        alt="React Logo"
+                    />
+                    <PageTitle />
+                    <h1>Test</h1>
+                </header>
+            </div>
+        );
+    }
 }
