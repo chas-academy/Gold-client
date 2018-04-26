c6d9d57367740dc4b46bfb438062be115c1b4e65
Alexander Grace <alexander.grace@chasacademy.se>
2018-04-26 12:41:27 +0200

    add top and bottom nav for employees

---
+++ src/components/menus/employee-top-nav/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { Nav, Navbar, NavDropdown, MenuItem }from 'react-bootstrap'
+
+import './style.css'
+
+class EmployeeTopNav extends Component {
+
+  render() {
+    return (
+            <div className="navBar">
+            <NavDropdown title={<i class="fas fa-list"></i>} noCaret>
+                <MenuItem>
+                    <Link to={`/admin/orders/incoming`}>
+                      <p>Nya</p>
+                    </Link>
+                </MenuItem>    
+                <MenuItem>
+                  <Link to={`/admin/orders/active`}>
+                    <p>Pågående</p>
+                  </Link>
+                </MenuItem>    
+                <MenuItem>
+                  <Link to={`/admin/orders/internal`}>
+                    <p>Internal</p>
+                  </Link>
+                </MenuItem>    
+                <MenuItem>
+                  <Link to={`/admin/orders/complaints`}>
+                    <p>Reklamationer</p>
+                  </Link>
+                </MenuItem>    
+                <MenuItem>
+                  <Link to={`/admin/orders/completed`}>
+                    <p>Avslutade</p>
+                  </Link>
+                </MenuItem>    
+            </NavDropdown>
+            <NavDropdown title={<i class="far fa-user-circle"></i>} noCaret>
+                <MenuItem>
+                  <Link to={`/admin/accounts/employees`}>
+                    <p>Anställda</p>
+                  </Link>
+                </MenuItem>  
+                <MenuItem>
+                  <Link to={`/admin/accounts/customers/private`}>
+                    <p>Privatkunder</p>
+                  </Link>
+                </MenuItem>  
+                <MenuItem>
+                  <Link to={`/admin/accounts/customers/companies`}>
+                    <p>Företagskunder</p>
+                  </Link>
+                </MenuItem>  
+                <MenuItem>
+                  <Link to={`/admin/accounts/profile`}>
+                    <p>Min profil</p>
+                  </Link>
+                </MenuItem>  
+              </NavDropdown>
+              </div>
+    );
+  }
+}
+
+export default withRouter(EmployeeTopNav);
