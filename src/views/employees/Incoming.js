e156eb8af8716f7818e40af7bd6ba2a8822bf082
Alexander Grace <alexander.grace@chasacademy.se>
2018-04-26 12:13:15 +0200

    add confirm, history, incoming views for employees

---
+++ src/views/employees/Incoming.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+
+export default class Incoming extends Component {
+
+  render() {
+    return (
+        <div> 
+            employee - Incoming view
+        </div>    
+    )
+  }
+
+}
