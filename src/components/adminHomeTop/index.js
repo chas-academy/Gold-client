2e9b749b42f6f3b8e7a7dbf6159168edcfdec92f
Eleni Nikou <eleni.nikou@chasacademy.se>
2018-05-03 21:23:43 +0200

    contact info updated

--- src/components/adminHomeTop/index.js
+++ src/components/adminHomeTop/index.js
@@ -1,10 +1,78 @@
 import React from "react";
 
-const AdminHomeTop = () => {
+import './style.css'
 
-    return (
-        <h3>Något fint</h3>
-    );
-  }
+const AdminHomeTop = () => {
+  return (
+    <div className="BasicList__container AdminHome">  
+        <h4> Dagens jobb </h4>
+      <ul>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/3">
+            <i
+              className="fas fa-map-marker button-glow-new"
+              style={{ color: "red", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Stena
+        </li>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/4">
+            <i
+              className="fas fa-map-marker button-glow-new"
+              style={{ color: "red", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Köpenhamnsvägen 5 
+        </li>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/1">
+            <i
+              className="fas fa-map-marker button-glow-active"
+              style={{ color: "orange", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Köpenhamnsvägen 50 : Giovanna
+        </li>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/2">
+            <i
+              className="fas fa-map-marker button-glow-active"
+              style={{ color: "orange", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Lorensborgsgatan 7 : Tommy
+        </li>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/2">
+            <i
+              className="fas fa-map-marker "
+              style={{ color: "green", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Baltzarsgatan 3 : Karin
+        </li>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/2">
+            <i
+              className="fas fa-map-marker "
+              style={{ color: "green", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Caroli : Ivan
+        </li>
+        <li className="AdminHomeTop__li">
+          <a href="admin/orders/2">
+            <i
+              className="fas fa-map-marker"
+              style={{ color: "green", fontSize: "18px", marginRight: '10px' }}
+            />
+          </a>{" "}
+           Södra förstadsgatan 7 : Tommy
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default AdminHomeTop;
--- src/components/googleApi/index.js
+++ src/components/googleApi/index.js
@@ -12,13 +12,13 @@ export default class MapContainer extends Component {
   
     static defaultProps = {
     center: { lat: 55.5873503, lng: 12.9814429},
-    zoom: 9
+    zoom: 9 
   }
 
 render() {
     return (
     <div className="google-map-overlay">
-      <div className='google-map' style={{ height: '100vh', width: '100%', marginTop: '-20px'}}>
+      <div className='google-map' style={{ height: '50vh', width: '100%', marginTop: '35vh'}}>
           <GoogleMapReact
             bootstrapURLKeys={{ key: Google }}
             defaultCenter={ this.props.center }
@@ -27,25 +27,25 @@ render() {
             <AnyReactComponent
               lat={ 55.60587 }
               lng={ 13.20073 }
-              text={<a href="admin/orders/1"><i className="fas fa-dot-circle button-glow-new" style={{color:'red', fontSize: '16px'}}></i></a>}
+              text={<a href="admin/orders/1"><i className="fas fa-map-marker button-glow-new" style={{color:'red', fontSize: '18px'}}></i></a>}
             />
 
               <AnyReactComponent
                 lat={ 55.70587 }
                 lng={ 13.30073 }
-                text={<a href="admin/orders/2"> <i className="fas fa-dot-circle button-glow-active" style={{color:'orange', fontSize: '16px'}}></i></a>}
+                text={<a href="admin/orders/2"> <i className="fas fa-map-marker button-glow-active" style={{color:'orange', fontSize: '18px'}}></i></a>}
               />
 
             <AnyReactComponent
               lat={ 55.80587 }
               lng={ 13.30073 }
-              text={<a href="admin/orders/3"><i className="fas fa-dot-circle" style={{color:'green', fontSize: '16px'}}></i></a>}
+              text={<a href="admin/orders/3"><i className="fas fa-map-marker" style={{color:'green', fontSize: '18px'}}></i></a>}
             />
 
           <AnyReactComponent
               lat={ 55.80587 }
               lng={ 12.99073 }
-              text={<a href="admin/orders/4"><i className="fas fa-dot-circle" style={{color:'green', fontSize: '16px'}}></i></a>}
+              text={<a href="admin/orders/4"><i className="fas fa-map-marker" style={{color:'green', fontSize: '18px'}}></i></a>}
             />
 
           </GoogleMapReact>
