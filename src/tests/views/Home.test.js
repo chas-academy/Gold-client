90da1e9d18b0c0792f8f8db3c6d636b9196360fd
Alexander Grace <alexander.grace@chasacademy.se>
2018-05-17 14:32:08 +0200

    testing test with jest

--- src/tests/views/Home.test.js
+++ src/tests/views/Home.test.js
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from '../../../Views/Site/Home'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Home />, div)
-  ReactDOM.unmountComponentAtNode(div)
+// it('renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<Home />, div)
+//   ReactDOM.unmountComponentAtNode(div)
+// })
+
+
+describe('Auth reducer', () => {
+  it('has a default state',() => {
+    expect().toEqual({
+      isFetching: false,
+    })
+  })
 })
