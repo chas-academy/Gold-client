11c6b21143656e6767cda9a644d1d5aa0d0e1f34
Alexander Grace <alexander.grace@chasacademy.se>
2018-05-18 11:23:58 +0200

    add test for reducers and actions

---
+++ src/tests/reducers/Auth.test.js
@@ -0,0 +1,9 @@
+import AuthReducer from '../../src/redux/reducers'
+import 
+// it('renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<Home />, div)
+//   ReactDOM.unmountComponentAtNode(div)
+// })
+
+
