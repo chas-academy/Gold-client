3a2cd3db403b12c5bd4a7e09a782ecda6130b39e
patryk7rybaczek <patryk.rybaczek@chasacademy.se>
2018-05-21 16:09:07 +0200

    small css fixes here and there, removed transition in menu for better ux and fixed a bug with the menu on mobile

--- src/components/menus/admin-top-nav/Services.js
+++ src/components/menus/admin-top-nav/Services.js
@@ -16,7 +16,7 @@ class AdminTopServices extends Component {
   
       this.openMenuServices = this.openMenuServices.bind(this);
     }
-    componentDidMount() {
+    componentDidMount(event) {
       let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
       if(width > '800') {
         this.setState({ openServices: true })
@@ -44,10 +44,10 @@ class AdminTopServices extends Component {
         } else {
           this.setState({ openServices: true })
         }
+        
       }
 
       render() {
-
         const { openServices } = this.state;
         return (
     
